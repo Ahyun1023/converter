75296b5301b7da3fb10addbfc157b3d0b60e5438
AH HYEON NAM <ska6371@gmail.com>
2021-05-18 17:14:08 +0900

    자바로 만들려던 거 node.js로 변경

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+
+const static = require('serve-static');
+
+const expressErrorHandler = require('express-error-handler');
+
+app.use('port', process.env.PORT || 3000);
+app.set('view engine', 'pug');
+app.set('views, views');
+
+router = express.Router();
+
+router.get('/', function(req, res, next) {
+    //Do whatever...
+});
+
+app.use('/', router);
+
+let errorHandler = expressErrorHandler({
+    static: {
+        '404': './public/404.html'
+    }
+});
+
+app.use(expressErrorHandler.httpError(404));
+app.use(errorHandler);
+
+http.createServer(app).listen(app.get('port'), ()=>{
+    console.log('서버 시작, 포트넘버: %d', app.get('port'));
+})
