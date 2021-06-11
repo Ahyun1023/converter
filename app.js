ce74a8d03cdbc73e5cd8d5a55b9b01c99851eb92
남아현 <ska6371@gmail.com>
2021-06-11 23:01:29 +0900

    서버 연결..

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 
-
 const app = express();
 
 const static = require('serve-static');
@@ -24,14 +23,14 @@ app.use('/', routes);
 app.use(app.router);
 routes.initialize(app);
 
-let errorHandler = expressErrorHandler({
+/*let errorHandler = expressErrorHandler({
     static: {
         '404': './public/404.html'
     }
-});
+});*/
 
-app.use(expressErrorHandler.httpError(404));
-app.use(errorHandler);
+/*app.use(expressErrorHandler.httpError(404));
+app.use(errorHandler);*/
 
 http.createServer(app).listen(app.get('port'), ()=>{
     console.log('서버 시작, 포트넘버: %d', app.get('port'));
