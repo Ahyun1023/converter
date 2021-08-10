de0230b62f92c89f0726215f1f7c9963046fb36d
남아현 <ska6371@gmail.com>
2021-08-10 22:06:01 +0900

    choice1 click event

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,11 @@ function convert(){
     let beforeText = document.getElementById('beforeText').value;
     
 }
+
+function choice1Click(){
+    $(".mainDiv").load("/input.do");
+
+    $(".mainDiv").load("/input.do", {"ex" : "data"}, function(){
+        alert("success");
+    });
+}
