2393f27be24c607b88fc11fa23ffa833415884ea
남아현 <ska6371@gmail.com>
2021-09-14 21:55:32 +0900

    node 통신

--- public/js/common.js
+++ public/js/common.js
@@ -9,8 +9,24 @@ function loginButtonClick(){
 function doLogin(){
     let login_data = {
         id: document.getElementById("id").value,
-        pw: doucment.getElementById("pw").value
+        password: document.getElementById("password").value
     }
 
     login_data = JSON.stringify(login_data);
+
+    $(document).ready(()=>{
+        $.ajax({
+            url: '/doLogin',
+            type: 'POST',
+            dataType: 'json',
+            data: {login_data: login_data},
+            success: (result)=>{
+                if(result.result == true){
+                    location.href = '/public/index.html';
+                } else{
+                    alert('아이디 혹은 비밀번호가 틀렸습니다.');
+                }
+            }
+        })
+    });
 }
--- routes/common_route.js
+++ routes/common_route.js
@@ -1,7 +1,8 @@
-const mysql_dbc = require('../DB/db')();
-const connection = mysql_dbc.init();
+//const mysql_dbc = require('../DB/db')();
+//const connection = mysql_dbc.init();
 
 const doLogin = (req, res)=>{
+    res.send({result: true});
 };
 
 module.exports.doLogin = doLogin;
