d4360cad23a82f637929bd44d775fb0a8974a6c0
AH HYEON NAM <ska6371@gmail.com>
2022-05-03 16:49:54 +0900

    카멜, 스네이크 ,파스칼, 케밥 표기

--- public/js/input.js
+++ public/js/input.js
@@ -3,7 +3,113 @@ function convertReset(){
     document.getElementById('afterConverTextarea').value = '';
 }
 
-function convertChange(){
-    console.log(document.getElementById('beforeConverTextarea').value);
-    document.getElementById('beforeConverTextarea').value = document.getElementById('afterConverTextarea').value;
+function convertChange(enter){
+    let convertType = document.querySelector('input[name="convertType"]:checked').value;
+
+    if(convertType == 'camel' || convertType == 'pascal'){
+        camelOrPascalChange(enter, convertType);
+    } else if(convertType == 'snake'){
+        snakeChange(enter);
+    } else if(convertType == 'kebab'){
+        kebabChange(enter);
+    }
+}
+
+function typeChange(){
+    let convertType = document.querySelector('input[name="convertType"]:checked').value;
+
+    if(convertType == 'camel'){
+        document.getElementById('divTitle').innerHTML = '카멜 표기법';
+        document.getElementById('divContent').innerHTML = '띄어쓰기, 언더바를 변환합니다. (ex: camel_var -> camelVar)';
+    } else if(convertType == 'snake'){
+        document.getElementById('divTitle').innerHTML = '스네이크 표기법';
+        document.getElementById('divContent').innerHTML = '띄어쓰기를 변환합니다. (ex: snake var -> snake_var 혹은 SNAKE_VAR)';
+    } else if(convertType == 'pascal'){
+        document.getElementById('divTitle').innerHTML = '파스칼 표기법';
+        document.getElementById('divContent').innerHTML = '띄어쓰기, 언더바를 변환합니다. (ex: pascal_var -> PascalVar)';
+    } else if(convertType == 'kebab'){
+        document.getElementById('divTitle').innerHTML = '케밥 표기법';
+        document.getElementById('divContent').innerHTML = '띄어쓰기, 언더바를 변환합니다. (kebab_var -> kebab-var)';
+    }
+
+    if(convertType == 'snake'){
+        document.getElementById('TypeForm').style.display = 'block';
+    } else {
+        document.getElementById('TypeForm').style.display = 'none';
+    }
+
+    convertChange(document.getElementById('beforeConverTextarea').value);
+}
+
+function camelOrPascalChange(enter, convertType){
+    let enterTextLength = enter.length;
+    let afterText = '';
+    let beforeUnderbar = false;
+
+    for(var i = 0; i < enterTextLength; i++){
+        var beforeText = enter.substr(i - 1, 1);
+        var nowText = enter.substr(i, 1);
+
+        /* 파스칼은 앞글자를 대문자로 표기한다 */
+        if(convertType == 'pascal'){
+            if(i == 0 || beforeText == '\n'){
+                nowText = nowText.toUpperCase();
+            }
+        }
+
+        if(nowText == '_' || nowText == ' '){
+            beforeUnderbar = true;
+            continue;
+        }
+
+        if(beforeUnderbar == true){
+            nowText = nowText.toUpperCase();
+            beforeUnderbar = false;
+        }
+        afterText += nowText;
+    }
+    
+    document.getElementById('afterConverTextarea').value = afterText;
+}
+
+function snakeChange(enter){
+    /* 대소문자 구분 필요 */
+    let enterTextLength = enter.length;
+    let afterText = '';
+    let caseType = document.querySelector('input[name="caseType"]:checked').value;
+
+    for(var i = 0; i < enterTextLength; i++){
+        var nowText = enter.substr(i, 1);
+
+        if(nowText == ' '){
+            nowText = '_';
+        } else {
+            if(caseType == 'enBig'){
+                nowText = nowText.toUpperCase();
+            } else{
+                nowText = nowText.toLowerCase();
+            }
+        }
+
+        afterText += nowText;
+    }
+    
+    document.getElementById('afterConverTextarea').value = afterText;
+}
+
+function kebabChange(enter){
+    let enterTextLength = enter.length;
+    let afterText = '';
+
+    for(var i = 0; i < enterTextLength; i++){
+        var nowText = enter.substr(i, 1);
+
+        if(nowText == '_' || nowText == ' '){
+            nowText = '-';
+        }
+
+        afterText += nowText;
+    }
+    
+    document.getElementById('afterConverTextarea').value = afterText;
 }
