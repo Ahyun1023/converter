fb228997b1c517e8c1362242046acbd61e9faea0
AH HYEON NAM <ska6371@gmail.com>
2022-05-09 17:58:05 +0900

    유닉스 시간 변경 오류 수정 및 유효성검사

--- public/js/convert/uniConvert.js
+++ public/js/convert/uniConvert.js
@@ -1,3 +1,5 @@
+const NUMBER_CHECK = /^[0-9]+$/;
+
 function typeChange(){
     let convertType = document.querySelector('input[name="convertType"]:checked').value;
 
@@ -82,9 +84,21 @@ function convertUnixTime(isNowTime){
     let formType = document.querySelector('input[name="caseType"]:checked').value;
     
     if(formType == 'normalChange'){
-        if(isNowTime){
-            document.getElementById('afterConverTextarea').value = Math.floor(new Date().getTime() / 1000);
+        if(isNowTime == true){
+            let nowNormalTime = new Date();
+            document.getElementById('normalYear').value = nowNormalTime.getFullYear();
+            document.getElementById('normalMonth').value = nowNormalTime.getMonth() + 1;
+            document.getElementById('normalDay').value = nowNormalTime.getDate();
+            document.getElementById('normalHour').value = nowNormalTime.getHours();
+            document.getElementById('normalMinutes').value = nowNormalTime.getMinutes();
+            document.getElementById('normalSeconds').value = nowNormalTime.getSeconds();
+
+            document.getElementById('afterConverTextarea').value = Math.floor(nowNormalTime.getTime() / 1000);
         } else {
+            if(!checkAndFillZero()){
+                return;
+            }
+
             let year = document.getElementById('normalYear').value;
             let month = document.getElementById('normalMonth').value;
             let day = document.getElementById('normalDay').value;
@@ -98,18 +112,67 @@ function convertUnixTime(isNowTime){
             document.getElementById('afterConverTextarea').value = enter;
         }
     } else {
-        let nowUnixTime = Math.floor(new Date().getTime() / 1000);
-        document.getElementById('beforeConverTextarea').value = nowUnixTime;
+        let beforeUnixTime;
+
+        if(isNowTime == true){
+            beforeUnixTime = Math.floor(new Date().getTime() / 1000);
+            document.getElementById('beforeConverTextarea').value = beforeUnixTime;
+        } else {
+            beforeUnixTime = document.getElementById('beforeConverTextarea').value;
+
+            if(!NUMBER_CHECK.test(beforeUnixTime)){
+                alert('숫자만 입력 가능합니다.');
+                return false;
+            }
+        }
+
+        beforeUnixTime = new Date(beforeUnixTime*1000);
+
+        let year = beforeUnixTime.getFullYear();
+        let month = beforeUnixTime.getMonth() + 1;
+        let day = beforeUnixTime.getDate();
+        let hour = beforeUnixTime.getHours();
+        let minutes = beforeUnixTime.getMinutes();
+        let seconds = beforeUnixTime.getSeconds();
         
-        nowUnixTime = new Date(nowUnixTime*1000);
-
-        let year = nowUnixTime.getFullYear();
-        let month = nowUnixTime.getMonth() + 1;
-        let day = nowUnixTime.getDate();
-        let hour = nowUnixTime.getHours();
-        let minutes = nowUnixTime.getMinutes();
-        let seconds = nowUnixTime.getSeconds();
-
         document.getElementById('afterConverTextarea').value = year + "-" + month + "-" + day + " " + hour + ":" + minutes + ":" + seconds;
     }
+}
+
+function checkAndFillZero(){
+    
+    let dateArr = [document.getElementById('normalYear').value, document.getElementById('normalMonth').value,
+        document.getElementById('normalDay').value, document.getElementById('normalHour').value,
+        document.getElementById('normalMinutes').value, document.getElementById('normalSeconds').value];
+
+    for(var i = 0; i < dateArr.length; i++){
+        if(dateArr[i] == '' || dateArr[i] == undefined){
+            alert('입력되지 않은 항목이 있습니다.');
+            return false;
+        }
+
+        if(!NUMBER_CHECK.test(dateArr[i])){
+            alert('날짜는 숫자만 입력 가능합니다.');
+            return false;
+        }
+
+        if(i == 0 && dateArr[i].length != 4){
+            alert('년도는 4자리수여야 합니다.');
+            return false;
+        } else if(i == 1 && dateArr[i] > 12 && dateArr[i] <= 0){
+            alert('월은 1 ~ 12월까지만 입력할 수 있습니다.');
+            return false;
+        } else if(i == 2 && dateArr[i] > 31 && dateArr[i] <= 0){
+            alert('날짜는 1 ~ 31일까지만 입력할 수 있습니다.');
+            return false;
+        } else if(i == 3 && dateArr[i] > 23 && dateArr[i] < 0){
+            alert('시간은 0 ~ 23시까지만 입력할 수 있습니다.');
+            return false;
+        } else if((i == 4 || i == 5) && dateArr[i] > 59 && dateArr[i] < 0){
+            alert('분 및 초는 0 ~ 59까지만 입력할 수 있습니다.');
+            return false;
+        }
+    }
+
+    return true;
 }
