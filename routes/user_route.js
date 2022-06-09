3c0d69e602d57d9e4132b378c9ea5219c1eae363
남아현 <ska6371@gmail.com>
2022-06-09 20:44:53 +0900

    user 테이블명 변경되어 쿼리 수정

--- routes/user_route.js
+++ routes/user_route.js
@@ -8,8 +8,8 @@ const doLogin = (req, res)=>{
 
     loginInfo = JSON.parse(loginInfo);
     loginInfo.password = crypto.createHash('sha256').update(loginInfo.password).digest('hex');
-    
-    connection.query('SELECT * FROM user WHERE id = ? AND password = ?;', [loginInfo.id, loginInfo.password], (err, results)=>{
+
+    connection.query('SELECT * FROM USER_TB WHERE ID = ? AND PW = ?;', [loginInfo.id, loginInfo.password], (err, results)=>{
         if(err){
             console.log(err);
         } else{
