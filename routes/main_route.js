f70cf3d1f4995ddb4f5815617876a38207be81a7
남아현 <ska6371@gmail.com>
2022-06-15 22:37:01 +0900

    변환화면 이동 추가

--- routes/main_route.js
+++ routes/main_route.js
@@ -12,4 +12,19 @@ router.get('/', function(req, res){
     res.render('index');
 });
 
+/* caseConvert로 이동 */
+router.get('/caseConvert', function(req, res){
+    res.render('caseConvert');
+})
+
+/* uniConvert로 이동 */
+router.get('/uniConvert', function(req, res){
+    res.render('uniConvert');
+})
+
+/* queryConvert로 이동 */
+router.get('/queryConvert', function(req, res){
+    res.render('queryConvert');
+})
+
 module.exports = router;
